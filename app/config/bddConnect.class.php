d647af1476530e3c93030999633da71b66da3e9e
thebestcrac@gmail.com <thebestcrac@gmail.com>
2019-10-09 12:02:08 +0000

    ...

--- app/config/bddConnect.class.php
+++ app/config/bddConnect.class.php
@@ -8,7 +8,7 @@
         public static function getPDO(): PDO{
 
             if (self::$PDOInstance == null) {
-                self::$PDOInstance = new PDO('mysql:host=127.0.0.1;dbname=rendezVous;charset=utf8', 'root', 'root', [
+                self::$PDOInstance = new PDO('mysql:host=127.0.0.1;dbname=dalal_jamm;charset=utf8', 'miko', 'BC malick92', [
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
                 ]);
